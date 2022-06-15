014f5af1b73bc5870a3aa011c756137fb64030cf
Victor Flores <vfloresp@pop-os.localdomain>
2022-06-15 00:19:28 -0500

    nvim configuration files

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,24 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0  
+vim.opt.fileencoding = "utf-8"
+vim.opt.ignorecase = true 
+vim.opt.mouse = "a" 
+vim.opt.hlsearch = true
+vim.opt.showmode = false
+vim.opt.smartcase = true
+vim.opt.smartindent = true 
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8 
+vim.opt.colorcolumn="80"
+
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[hi ColorColumn ctermbg=grey guibg=grey]]
