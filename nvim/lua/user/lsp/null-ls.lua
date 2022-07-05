788a10474e0a62a3333254614cf9a64f9b847a17
Victor Flores <vfloresp@pop-os.localdomain>
2022-07-04 21:52:53 -0500

    updated null ls

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,11 +9,13 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier,
-    formatting.black,
-    formatting.stylua,
-    diagnostics.flake8,
-  },
+    debug = false,
+    sources = {
+        formatting.prettier,
+        formatting.black,
+        formatting.stylua,
+        diagnostics.flake8.with({
+            extra_args = {"--max-line-height", "88"}
+        })
+    },
 }
