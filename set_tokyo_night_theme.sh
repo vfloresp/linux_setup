eaeca0af971711fcdeb81555472df6c3c25301a1
Victor Flores <vfloresp@pop-os.localdomain>
2022-07-18 18:09:42 -0500

    tokyo night theme update

--- set_tokyo_night_theme.sh
+++ set_tokyo_night_theme.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme.git
-cd Tokyo-Night-GTK-Theme/
 
 mkdir -p ~/.local/share/themes/
 mkdir -p ~/.local/share/icons/
 
-cp -r ./themes/Tokyonight-Dark-BL/ ~/.local/share/themes/
-cp -r ./icons/tokyonight-dark/ ~/.local/share/icons/
+cp -r Tokyo-Night-GTK-Theme/themes/Tokyonight-Dark-BL/ ~/.local/share/themes/
+cp -r Tokyo-Night-GTK-Theme/icons/tokyonight_dark/ ~/.local/share/icons/
+cp -r Tokyo-Night-GTK-Theme/wallpapers ~/Pictures/
+
+rm -r Tokyo-Night-GTK-Theme
