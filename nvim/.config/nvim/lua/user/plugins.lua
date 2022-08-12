77e051686942f385899d306ff5bad955f804d3f9
Victor Flores <vfloresp@pop-os.localdomain>
2022-08-12 13:53:44 -0500

    updated config

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -39,55 +39,55 @@ return packer.startup(function(use)
      use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
      use "numToStr/Comment.nvim" -- Easily comment stuff
      use "kyazdani42/nvim-web-devicons"
---     use {
---         "kyazdani42/nvim-tree.lua",
---         requires = {
---             "kyazdani42/nvim-tree.lua"
---         }
---     }
---     use "akinsho/bufferline.nvim"
---     use "moll/vim-bbye"
---     use "akinsho/toggleterm.nvim"
---     use 'nvim-lualine/lualine.nvim'
---     use "lukas-reineke/indent-blankline.nvim"
--- 
---     -- coloschemes
+     use {
+         "kyazdani42/nvim-tree.lua",
+         requires = {
+             "kyazdani42/nvim-tree.lua"
+         }
+     }
+     use "akinsho/bufferline.nvim"
+     use "moll/vim-bbye"
+     use "akinsho/toggleterm.nvim"
+     use 'nvim-lualine/lualine.nvim'
+     use "lukas-reineke/indent-blankline.nvim"
+ 
+     -- coloschemes
      use "lunarvim/colorschemes"
      use 'folke/tokyonight.nvim'
--- 
---     -- autocomplete
---     use "hrsh7th/nvim-cmp" -- The completion plugin
---     use "hrsh7th/cmp-buffer" -- buffer completions
---     use "hrsh7th/cmp-path" -- path completions
---     use "hrsh7th/cmp-cmdline" -- cmdline completions
---     use "saadparwaiz1/cmp_luasnip" -- snippet completions
---     use "hrsh7th/cmp-nvim-lsp"
---     use "hrsh7th/cmp-nvim-lua"
--- 
---     -- snippets
---     use "L3MON4D3/LuaSnip" --snippet engine
---     use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
--- 
---     -- LSP
---     use "neovim/nvim-lspconfig" -- enable LSP
---     use "williamboman/nvim-lsp-installer" -- simple to use language server installer
---     use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
--- 
---     -- Telescope
---     use "nvim-telescope/telescope.nvim"
---     use 'nvim-telescope/telescope-media-files.nvim'
--- 
---     -- Treesitter
---     use {
---         "nvim-treesitter/nvim-treesitter",
---         run = ":TSUpdate",
---     }
---     use "p00f/nvim-ts-rainbow"
---     use 'JoosepAlviste/nvim-ts-context-commentstring'
--- 
---     -- Git
---     use "lewis6991/gitsigns.nvim"
--- 
+ 
+     -- autocomplete
+     use "hrsh7th/nvim-cmp" -- The completion plugin
+     use "hrsh7th/cmp-buffer" -- buffer completions
+     use "hrsh7th/cmp-path" -- path completions
+     use "hrsh7th/cmp-cmdline" -- cmdline completions
+     use "saadparwaiz1/cmp_luasnip" -- snippet completions
+     use "hrsh7th/cmp-nvim-lsp"
+     use "hrsh7th/cmp-nvim-lua"
+ 
+     -- snippets
+     use "L3MON4D3/LuaSnip" --snippet engine
+     use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+ 
+     -- LSP
+     use "neovim/nvim-lspconfig" -- enable LSP
+     use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+     use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+ 
+     -- Telescope
+     use "nvim-telescope/telescope.nvim"
+     use 'nvim-telescope/telescope-media-files.nvim'
+ 
+     -- Treesitter
+     use {
+         "nvim-treesitter/nvim-treesitter",
+         run = ":TSUpdate",
+     }
+     use "p00f/nvim-ts-rainbow"
+     use 'JoosepAlviste/nvim-ts-context-commentstring'
+ 
+     -- Git
+     use "lewis6991/gitsigns.nvim"
+ 
      -- Automatically set up your configuration after cloning packer.nvim
      -- Put this at the end after all plugins
      if PACKER_BOOTSTRAP then
