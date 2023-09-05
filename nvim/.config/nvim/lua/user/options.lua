b663d2935cbcde7273a4d1030323090e3adb3899
vfloresp <vfloresp@pop-os.localdomain>
2023-09-04 21:07:44 -0700

    min version of options

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,10 +1,7 @@
 vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.conceallevel = 0  
-vim.opt.fileencoding = "utf-8"
+-- vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.ignorecase = true 
 vim.opt.mouse = "a" 
-vim.opt.hlsearch = true
 vim.opt.showmode = false
 vim.opt.smartcase = true
 vim.opt.termguicolors = true
@@ -20,6 +17,6 @@ vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.scrolloff = 8 
 vim.opt.colorcolumn="88"
+vim.opt.iskeyword = vim.opt.iskeyword + "-"
 
-vim.cmd [[set iskeyword+=-]]
 vim.cmd [[hi ColorColumn ctermbg=grey guibg=grey]]
